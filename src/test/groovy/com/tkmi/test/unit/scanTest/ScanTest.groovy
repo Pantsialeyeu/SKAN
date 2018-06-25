2b2070cf1bbce73e0624a6d5a2d61b7261fe8b8c
pavelpantseleyeu <pavel_pantseleyeu@epam.com>
2018-06-25 09:19:44 +0300

    add test3

--- src/test/groovy/com/tkmi/test/unit/scanTest/ScanTest.groovy
+++ src/test/groovy/com/tkmi/test/unit/scanTest/ScanTest.groovy
@@ -1,5 +1,6 @@
 package com.tkmi.test.unit.scanTest
 
+import groovy.json.JsonBuilder
 import spock.lang.Specification
 
 class ScanTest extends Specification {
@@ -96,5 +97,67 @@ class ScanTest extends Specification {
 		assert y == Ymax + 1
 		and: 'actual coverage should match expected coverage'
 		assert actualCoverage == expectedCoverage
+	}
+	
+	def 'test3'(){
+		given: 'area'
+		def Xmin = 0
+		def Ymin = 0
+		def Xmax = 10
+		def Ymax = 10
+		and: 'initial position'
+		def x = 0
+		def y = 0
+		and: 'expected coverage'
+		def expectedCoverage = []
+		(Ymin..Ymax).each{ y_pos ->
+			expectedCoverage.add('y' + y_pos)
+			(Xmin..Xmax).each{ x_pos ->
+				expectedCoverage.add('x' + x_pos)
+			}
+		}
+		when: 'execution'
+		def actualCoverage = []
+		while (!(y > Ymax)) {
+			actualCoverage.add('y' + y)
+			x = 0
+			actualCoverage.add('x' + x)
+
+			while (!(x == Xmax)) {
+				
+				// sensor block
+				def value = getU()
+				
+				// Workstation block
+				sendToPc(x, y, value)
+
+				x++
+				
+				actualCoverage.add('x' + x)
+			}
+			y++
+		}
+		then: 'final position should match expected'
+		assert x == Xmax
+		assert y == Ymax + 1
+		and: 'actual coverage should match expected coverage'
+		assert actualCoverage == expectedCoverage
+		println new JsonBuilder().toPrettyString(output)
+	}
+	
+	// Sensor
+	
+	def getU(){
+		new Random().nextDouble() // simulates U measurement
+	}
+	
+	// Workstation integration
+	
+	def counter = 0
+	def output = []
+	
+	def sendToPc(x, y, u){
+		output.add(["$counter": ['x': x, 'y': y, 'u': getU()]])
+		counter++
 	}
 }
